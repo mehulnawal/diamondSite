4c94ba76cd8cec0b9f050e4a4332830b6f244752
Mehul Nawal <your.email@example.com>
2025-10-02 15:44:53 +0530

    created Firebase file

---
+++ src/Components/Global/Firebase.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyDUfQ-2dCuddeuBe4BxkUgmI740edBUelo",
+    authDomain: "diamondsite-02102025.firebaseapp.com",
+    projectId: "diamondsite-02102025",
+    storageBucket: "diamondsite-02102025.firebasestorage.app",
+    messagingSenderId: "69215560595",
+    appId: "1:69215560595:web:a1e1cf0f927d030f087e66",
+    measurementId: "G-Z4Q2Q8KBY1"
+};
+
+export const Firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
